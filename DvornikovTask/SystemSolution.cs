70a9c689b6ffc98681e7042dcb90bfc43f667e38
Nikolay Bely <belyynikolay@gmail.com>
2019-07-04 23:41:09 +0700

    Fix formatting

--- DvornikovTask/SystemSolution.cs
+++ DvornikovTask/SystemSolution.cs
@@ -1,20 +1,18 @@
-using System.Dynamic;
-
 namespace DvornikovTask
-{
-    public class SystemSolution
-    {
-        public uint DimensionsCount { get; }
-        public uint[] Values { get; }
-
-        public uint[] Indexes { get; }
-
-        public SystemSolution(uint dimension, uint[] values, uint[] indexes)
-        {
-            DimensionsCount = dimension;
-            Values = values;
-            Indexes = indexes;
-        }
-        
-    }
-}
+ {
+     public class SystemSolution
+     {
+         public uint DimensionsCount { get; }
+         public uint[] Values { get; }
+ 
+         public uint[] Indexes { get; }
+ 
+         public SystemSolution(uint dimension, uint[] values, uint[] indexes)
+         {
+             DimensionsCount = dimension;
+             Values = values;
+             Indexes = indexes;
+         }
+         
+     }
+ }
